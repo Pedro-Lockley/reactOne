fda40606da06364dcaae7eda088485152fff70a0
Pedro-Lockley <pedro.lockley@hotmail.com>
2017-09-04 17:28:37 -0300

    Commit 1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import Teste from './teste';
+ReactDOM.render(<Teste />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/teste.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './teste.css';
+
+class Teste extends Component {
+  render() {
+    return (
+      <div id="caixa">
+      <h1>Essa foi a lição de casa passada por Alex, fazer uma caixa com cantos arredondados e subir o código para o github.</h1>
+      <h2>Lição dada é lição comprida!</h2>
+      </div>
+    )
+  }
+}
+export default Teste;
